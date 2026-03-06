936910a63da5fb6d9b47af0215bc13c8b10419f2
Beatriz C. <bcavalcantii19@gmail.com>
2026-03-06 16:11:40 -0300

    Add files via upload

---
+++ matriz.py
@@ -0,0 +1,13 @@
+matriz = [
+    [1, 2, 3]
+    [4, 5, 6]
+    [7, 8, 9]
+]
+
+print("Matriz completa: ")
+for linha in matriz: 
+    print(linha)
+
+print(f"\n Elementos [1][2]: {matriz[1][2]}")
+print(f"\n Elementos [0][0]: {matriz[0][0]}")
+print(f"\n Elementos [2][1]: {matriz[2][1]}")
